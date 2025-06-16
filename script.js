b31a6d4788e2df1e3f356392cbf09516c3c62b2f
Mimi-SE-dotcom <abiolamariam247@gmail.com>
2025-06-16 14:50:29 +0100

    TOdo list

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
         const removeBtn = document.createElement('button');
         removeBtn.textContent = 'Remove';
-        removeBtn.className = 'remove-btn';
+        removeBtn.classList.add('remove-btn');
 
         // Remove task on button click
         removeBtn.onclick = () => {
